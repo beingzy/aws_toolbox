5d82c46cf62008a1fa4c328dd649f9d0fd9bac33
Yi Zhang <yi.zhang@atos.net>
2016-03-10 22:13:39 -0500

    format code

--- s3_functions.py
+++ s3_functions.py
@@ -10,14 +10,13 @@
 
 import os
 import sys
-import re  
 import threading
 import boto3
 from boto3.s3.transfer import S3Transfer
 
 def s3_list_all_buckets(print_out=False):
     """ list all buckets' name of associated account 
-	"""
+    """
     s3 = boto3.resource('s3')
     bucket_names = []
     for ii, bucket in enumerate(s3.buckets.all()):
@@ -25,10 +24,11 @@ def s3_list_all_buckets(print_out=False):
         if print_out:
             print( "-- {}: {} \n".format(ii+1, bucket.name) )
     return( bucket_names )
-	
+
+
 def s3_create_new_bucket(name, **kwargs):
     """ create a new bucket 
-	"""
+    """
     s3 = boto3.resource('s3')
     # check if bucket exists already
     exist_buckets = s3_list_all_buckets(print_out=False)
@@ -38,7 +38,8 @@ def s3_create_new_bucket(name, **kwargs):
     # create bucket
     s3.create_bucket(Bucket=name, **kwargs)
     return 
-	
+
+
 def s3_upload_data(file_path, bucket_name, folder_name='', **kwargs):
     """ create a new object in S3's specified bucket 
         to store provided local data 
@@ -49,9 +50,9 @@ def s3_upload_data(file_path, bucket_name, folder_name='', **kwargs):
     # and multiple file paths
     if type(file_path) != 'list':
         file_path = [file_path]
-		
+
     tot_files = len(file_path)
-	
+
     for ii, fpath in enumerate(file_path):
         extracted_file_name = fpath.split(os.sep)[-1]
         object_path = folder_name + '/' + extracted_file_name
@@ -59,15 +60,17 @@ def s3_upload_data(file_path, bucket_name, folder_name='', **kwargs):
         transfer.upload_file( filename=fpath, bucket=bucket_name, key=object_path, 
             callback=ProgressPercentage(fpath) )	
     return
-	
+
+
 def s3_download_data(file_path, bucket_name, object_name, **kwargs):
     """ download s3's object and save it to file_path
-	"""
-    tansfer = S3Transfer(boto3.client('s3'))
+    """
+    transfer = S3Transfer(boto3.client('s3'))
     transfer.download_file( filename=file_path, bucket=bucket_name, key=object_name, 
         callback = ProgressPercentage(file_path) )
     return
-	    
+
+
 class ProgressPercentage(object):
     """ class: provide call-back function to track progress
                transfer()
@@ -82,13 +85,15 @@ class ProgressPercentage(object):
 
     def __call__(self, bytes_amount):
         # to simplify we'll assuem this is hooked up 
-		# to a single filename. 
+        # to a single filename.
         with self._lock:
             self._seen_so_far += bytes_amount
             percentage = ( self._seen_so_far * 1.0 / self._size ) * 100
             sys.stdout.write(
-				"\rUploading: {} {:.2f} mb / {:.2f} mb (Progress: {:.2f}%, Speed: {:.2f} kb)".format(self._filename, self._seen_so_far / 1000000,
-				                               self._size / 1000000, percentage, bytes_amount / 1000)
+            "Uploading: {} {:.2f} mb / {:.2f} mb (Progress: {:.2f}%, Speed: {:.2f} kb)".format(self._filename,
+                                                                                               self._seen_so_far / 1000000,
+                                                                                               self._size / 1000000,
+                                                                                               percentage,
+                                                                                               bytes_amount / 1000)
             )
             sys.stdout.flush()
-		
